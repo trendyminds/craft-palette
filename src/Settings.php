6c585cf9c8f55f415d39ff1ebe24df15df9ddb4a
Aaron Bushnell <aaron.bushnell@gmail.com>
2022-02-18 08:52:38 -0500

    Setup ability to disable the plugin on the frontend or backend
    https://github.com/trendyminds/craft-palette/discussions/8

---
+++ src/Settings.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace trendyminds\palette;
+
+use craft\base\Model;
+
+class Settings extends Model
+{
+	/**
+	 * @var bool If Palette should be enabled on the frontend of the website
+	 */
+	public $enableOnFrontend = true;
+
+	/**
+	 * @var bool If Palette should be enabled on the backend of the website
+	 */
+	public $enableOnBackend = true;
+}
