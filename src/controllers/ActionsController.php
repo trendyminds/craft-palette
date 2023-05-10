70cd8dedc6388102bcb2ed12dbb9d67a5b0c4dc1
Aaron Bushnell <aaron.bushnell@gmail.com>
2023-05-10 11:25:04 -0400

    Return an empty array if no element was found

--- src/controllers/ActionsController.php
+++ src/controllers/ActionsController.php
@@ -210,6 +210,11 @@ class ActionsController extends Controller
 			// Find the matching element in Craft
 			$element = Craft::$app->getElements()->getElementByUri($uri);
 
+			// Return an empty array if no element was found
+            if (! $element) {
+                return [];
+            }
+
 			return [
 				[
 					'name' => $element->title,
